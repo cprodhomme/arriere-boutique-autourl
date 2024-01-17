8e4f34735004d7e413b8dc01826659c719bfff70
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-01-17 09:47:35 +0100

    ✨ Introduce AutoUrlHelper with some helpers
    index_path_for
    index_url_for
    new_path_for
    ...

---
+++ app/helpers/auto_url_helper.rb
@@ -0,0 +1,53 @@
+module AutoUrlHelper
+  # create methods :
+  # index_path_for
+  # index_url_for
+  # new_path_for
+  # ...
+  [:index, :new, :create, :show].each do |action|
+    define_method("#{action}_path_for") do |record, prefix: nil, **additional_params|
+      helper_name = helper_name(record, action: action, suffix: "path", prefix: prefix)
+      send(helper_name, additional_params)
+    end
+  
+    define_method("#{action}_url_for") do |record, prefix: nil, **additional_params|
+      helper_name = helper_name(record, action: action, suffix: "url", prefix: prefix)
+      send(helper_name, additional_params)
+    end
+  end
+
+  # create methods :
+  # edit_path_for
+  # edit_url_for
+  # update_path_for
+  # ...
+  [:edit, :update, :destroy].each do |action|
+    define_method("#{action}_path_for") do |record, prefix: nil, **additional_params|
+      helper_name = helper_name(record, action: action, suffix: "path", prefix: prefix)
+      send(helper_name, record, additional_params)
+    end
+  
+    define_method("#{action}_url_for") do |record, prefix: nil, **additional_params|
+      helper_name = helper_name(record, action: action, suffix: "url", prefix: prefix)
+      send(helper_name, record, additional_params)
+    end
+  end
+
+  private
+
+  HELPER_NAME_WITHOUT_ACTION = [:index, :show, :creation, :update, :destroy]
+  def helper_name(record, action:, suffix:, prefix: nil)
+    record_path_name = record_path_name(record, action)
+    helper_name = "#{record_path_name}_#{suffix}"
+    helper_name = "#{prefix}_#{helper_name}" if prefix
+    helper_name = "#{action}_#{helper_name}" unless HELPER_NAME_WITHOUT_ACTION.include?(action.to_sym)
+    helper_name
+  end
+
+  def record_path_name(record, action)
+    klass = record.class != Class ? record.class : record
+    return klass.to_s.pluralize.underscore if action.to_sym == :index
+
+    klass.to_s.underscore
+  end
+end
---
+++ spec/helpers/auto_url_helper_spec.rb
@@ -0,0 +1,189 @@
+require 'rails_helper'
+
+describe AutoUrlHelper, type: :helper do
+  describe "index" do
+    describe "#index_path_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/new" do
+        expect(helper.index_path_for(user)).to eq(users_path)
+      end
+
+      context "with additional params" do
+        it "get /users/1/new?key=value" do
+          expect(helper.index_path_for(user, key: "value")).to eq(users_path(key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/new#title1" do
+          expect(helper.index_path_for(user, anchor: "title1")).to eq(users_path(anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/new#title1" do
+          expect(helper.index_path_for(user, prefix: "admin")).to eq(admin_users_path)
+        end
+      end
+
+      context "when record is a model class" do
+        it "get /users/1/new" do
+          expect(helper.index_path_for(User)).to eq(users_path)
+        end
+      end
+    end
+
+    describe "#index_url_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/new" do
+        expect(helper.index_url_for(user)).to eq(users_url)
+      end
+
+      context "with additional params" do
+        it "get /users/1/new?key=value" do
+          expect(helper.index_url_for(user, key: "value")).to eq(users_url(key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/new#title1" do
+          expect(helper.index_url_for(user, anchor: "title1")).to eq(users_url(anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/new#title1" do
+          expect(helper.index_url_for(user, prefix: "admin")).to eq(admin_users_url)
+        end
+      end
+
+      context "when record is a model class" do
+        it "get /users/1/new" do
+          expect(helper.index_url_for(User)).to eq(users_url)
+        end
+      end
+    end
+  end
+
+  describe "new" do
+    describe "#new_path_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/new" do
+        expect(helper.new_path_for(user)).to eq(new_user_path)
+      end
+
+      context "with additional params" do
+        it "get /users/1/new?key=value" do
+          expect(helper.new_path_for(user, key: "value")).to eq(new_user_path(key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/new#title1" do
+          expect(helper.new_path_for(user, anchor: "title1")).to eq(new_user_path(anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/new#title1" do
+          expect(helper.new_path_for(user, prefix: "admin")).to eq(new_admin_user_path)
+        end
+      end
+
+      context "when record is a model class" do
+        it "get /users/1/new" do
+          expect(helper.new_path_for(User)).to eq(new_user_path)
+        end
+      end
+    end
+
+    describe "#new_url_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/new" do
+        expect(helper.new_url_for(user)).to eq(new_user_url)
+      end
+
+      context "with additional params" do
+        it "get /users/1/new?key=value" do
+          expect(helper.new_url_for(user, key: "value")).to eq(new_user_url(key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/new#title1" do
+          expect(helper.new_url_for(user, anchor: "title1")).to eq(new_user_url(anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/new#title1" do
+          expect(helper.new_url_for(user, prefix: "admin")).to eq(new_admin_user_url)
+        end
+      end
+
+      context "when record is a model class" do
+        it "get /users/1/new" do
+          expect(helper.new_url_for(User)).to eq(new_user_url)
+        end
+      end
+    end
+  end
+
+  describe "edit" do
+    describe "#edit_path_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/edit" do
+        expect(helper.edit_path_for(user)).to eq(edit_user_path(user))
+      end
+
+      context "with additional params" do
+        it "get /users/1/edit?key=value" do
+          expect(helper.edit_path_for(user, key: "value")).to eq(edit_user_path(user, key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/edit#title1" do
+          expect(helper.edit_path_for(user, anchor: "title1")).to eq(edit_user_path(user, anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/edit#title1" do
+          expect(helper.edit_path_for(user, prefix: "admin")).to eq(edit_admin_user_path(user))
+        end
+      end
+    end
+
+    describe "#edit_url_for(record)" do
+      let(:user) { User.new id: 1 }
+
+      it "get /users/1/edit" do
+        expect(helper.edit_url_for(user)).to eq(edit_user_url(user))
+      end
+
+      context "with additional params" do
+        it "get /users/1/edit?key=value" do
+          expect(helper.edit_url_for(user, key: "value")).to eq(edit_user_url(user, key: "value"))
+        end
+      end
+
+      context "with anchor" do
+        it "get /users/1/edit#title1" do
+          expect(helper.edit_url_for(user, anchor: "title1")).to eq(edit_user_url(user, anchor: "title1"))
+        end
+      end
+
+      context "with prefix" do
+        it "get /admin/users/1/edit#title1" do
+          expect(helper.edit_url_for(user, prefix: "admin")).to eq(edit_admin_user_url(user))
+        end
+      end
+    end
+  end
+end
